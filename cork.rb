7ff3f268b71115bc0c4a3d207b811424d17003be
Reuven V Gonzales <ravenac95@users.noreply.github.com>
2017-10-15 22:24:21 -0700

    Update v0.3.0

--- cork.rb
+++ cork.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Cork < Formula
   homepage 'https://github.com/virtru/cork'
-  version '0.2.1'
+  version '0.3.0'
 
-  url 'https://github.com/virtru/cork/releases/download/v0.2.1/cork-macos-amd64.zip'
-  sha256 '991b7b1e15d60bdd4375ba7fc794cc6dfcc76c884d8a5f84a0f70cb60f936843'
+  url 'https://github.com/virtru/cork/releases/download/v0.3.0/cork-macos-amd64.zip'
+  sha256 '64b0dd0354abfef58e2615797d2dd04fba38f1b75651df326a8e3cc2102454e2'
 
   depends_on :arch => :intel
 
